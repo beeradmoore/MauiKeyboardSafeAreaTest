667a72d3d364bd85fb42a0aa39ae20418670de6a
Brad <brad@4pi.com.au>
2026-02-06 13:21:18 +1100

    initial commit

---
+++ MauiKeyboardSafeAreaTest/MauiKeyboardSafeAreaTest/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MauiKeyboardSafeAreaTest;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
