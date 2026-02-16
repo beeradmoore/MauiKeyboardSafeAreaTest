791f1fb929dd1c59758e44c3d689578666157291
Brad <brad@4pi.com.au>
2026-02-16 14:21:37 +1100

    Update MainActivity.cs

--- MauiKeyboardSafeAreaTest/MauiKeyboardSafeAreaTest/Platforms/Android/MainActivity.cs
+++ MauiKeyboardSafeAreaTest/MauiKeyboardSafeAreaTest/Platforms/Android/MainActivity.cs
@@ -8,5 +8,10 @@ namespace MauiKeyboardSafeAreaTest;
     ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode |
                            ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
-{
+{   
+    protected override void OnCreate(Bundle? savedInstanceState)
+    {
+        base.OnCreate(savedInstanceState);
+        //Window?.SetSoftInputMode(Android.Views.SoftInput.AdjustNothing);
+    }
 }
